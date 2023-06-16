01ad0959ae04d9514854c56b6a06c75225e38f32
Andreseliasbh <andreseliasbh@gmail.com>
2023-06-16 11:40:59 -0700

    Assigment 0-1 Andres Briceno

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def main():
-
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
+    print("Hello word")
+    print('CS7: Introduction to Computer Programming')
 
 
 if __name__ == '__main__':
     main()
+
+#Assingment 0-1 Andres Briceno
